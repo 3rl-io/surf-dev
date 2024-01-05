6478bcfa4aaf08d167dd4babde4f6ba26adbe3be
kiran <kiranmkota@gmail.com>
2024-01-05 12:06:48 -0800

    misc add prod, bg variables add spacepizza game change finance/nitter to finance/top-stories change welcome message

--- public/app.js
+++ public/app.js
@@ -1,20 +1,32 @@
-const appCtrl = {
-    background: 'assets/backgrounds/polarcity.jpg',
-    zones: [
+let zones = null;
+if (isProd) {
+    zones = [
         {template: 'home', hasScript: true, iconCls: 'fas fa-house-chimney'},
-        {template: 'video', hasScript: true, toolbarCls: 'solid-toolbar video-toolbar', iconCls: 'fas fa-tv'},
-        {template: 'audio', toolbarCls: 'solid-toolbar audio-toolbar', iconCls: 'fas fa-headphones'},
         {template: 'finance', iconCls: 'fas fa-dollar-sign'},
-        null,
-        null
-    ],
+        {template: 'pizza', toolbarCls: 'solid-toolbar black-toolbar', iconCls: 'fas fa-pizza-slice'}, null, null, null];
+} else {
+    zones = [
+        {template: '', hasScript: true, iconCls: 'fas fa-house-chimney'},
+        {template: 'video', hasScript: true, toolbarCls: 'solid-toolbar video-toolbar', iconCls: 'fas fa-tv'},
+        {template: 'audio', toolbarCls: 'solid-toolbar black-toolbar', iconCls: 'fas fa-headphones'},
+        {template: 'finance', iconCls: 'fas fa-dollar-sign'},
+        null, null];
+}
+
+
+const urlParams = new URLSearchParams(window.location.search),
+    bg = urlParams.get('bg');
+
+const appCtrl = {
+    background: bg || 'assets/backgrounds/newmiami.jpg',
+    zones,
     activeZone: 0,
     dimmerLevel: 0,
     cycleDimmer() {
         this.dimmerLevel = (this.dimmerLevel + 1) % 4
     },
     goToZone(index) {
-        if (index === 1 && this.activeZone === 1 && Alpine.store('video')) {
+        if (index === this.activeZone && Alpine.store('video')) {
             Alpine.store('video').state = 1;
         }
         spidgets.goToZone(index);
